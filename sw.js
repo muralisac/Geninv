c60412cc02f2d00458ea4fe52f6867219a0d91c1
smuralisac <tnfa.sales@gmail.com>
2026-03-22 15:06:38 +0530

    Module
    Module

--- sw.js
+++ sw.js
@@ -1,22 +1,30 @@
-const CACHE_NAME = 'totatri-erp-v1';
-const ASSETS = [
-  './',
+const CACHE_NAME = 'nellai-erp-v1';
+const urlsToCache = [
   './index.html',
-  './manifest.json'
+  './css/styles.css',
+  './js/1-firebase-config.js',
+  './js/2-ui-utils.js',
+  './js/3-auth.js',
+  './js/4-database.js',
+  './js/5-directory.js',
+  './js/6-inventory.js',
+  './js/7-transactions.js',
+  './js/8-pdf-engine.js',
+  './manifest.json',
+  './NNlogo-removebg-preview.png',
+  './logo-192.png'
 ];
 
-// Install Service Worker and cache core files
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Serve cached files when offline
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
