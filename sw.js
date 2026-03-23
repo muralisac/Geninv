f31145b96b33ef606803747b47822d1322d17aad
smuralisac <tnfa.sales@gmail.com>
2026-03-23 13:21:50 +0530

    Update index

--- sw.js
+++ sw.js
@@ -1,34 +1,15 @@
-<<<<<<< HEAD
-const CACHE_NAME = 'nellai-erp-v10';
-=======
-const CACHE_NAME = 'nellai-erp-v3';
->>>>>>> 2e12a3f559dc71f763dd056217930a972ae72bd2
-const urlsToCache = [
-  './index.html',
-  './css/styles.css',
-  './js/1-firebase-config.js',
-  './js/2-ui-utils.js',
-  './js/3-auth.js',
-  './js/4-database.js',
-  './js/5-directory.js',
-  './js/6-inventory.js',
-  './js/7-transactions.js',
-  './js/8-pdf-engine.js',
-  './manifest.json',
-  './NNlogo-removebg-preview.png',
-  './logo-192.png'
-];
+const CACHE_NAME = 'nellai-erp-v11';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', e => {
+    e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
 });
