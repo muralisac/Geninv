32a7b1c537f05442feeb2df04cdca4b34292dfeb
smuralisac <tnfa.sales@gmail.com>
2026-03-23 15:22:20 +0530

    Modularized code
    Created Index.html, stylesheet in css folder and Javascripts in js folder.

--- sw.js
+++ sw.js
@@ -1,13 +1,30 @@
-const CACHE_NAME = 'nellai-erp-v11';
+const CACHE_NAME = 'nellai-erp-v12';
+const urlsToCache = [
+  './index.html',
+  './css/styles.css',
+  './js/1-config.js',
+  './js/2-utils.js',
+  './js/3-auth-db.js',
+  './js/4-directory.js',
+  './js/5-inventory.js',
+  './js/6-cart-builder.js',
+  './js/7-pdf-engine.js',
+  './js/8-history-payouts.js',
+  './manifest.json',
+  './NNlogo-removebg-preview.png',
+  './logo-192.png'
+];
 
 self.addEventListener('install', e => {
     self.skipWaiting();
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('activate', e => {
     e.waitUntil(self.clients.claim());
 });
 
+// Network-first strategy to ensure you always get the latest code updates
 self.addEventListener('fetch', e => {
     e.respondWith(
         fetch(e.request).catch(() => caches.match(e.request))
